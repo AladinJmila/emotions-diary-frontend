c0b61a9aee51e0579905dd69a03665bfea7d92dd
Alaeddine Jmila <jmila.aladin@gmail.com>
2022-09-10 13:57:52 +0100

    Added Nabar and Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='atlas-emotions'>
+        <h3>The Atlas Of Emotions</h3>
+        <a href='http://atlasofemotions.org/' target='_blank'>
+          learn more about emotions from experts
+        </a>
+      </div>
+      <div className='main-container'>
+        <a href='/editor'>
+          <h2>Edit</h2>
+        </a>
+        <a href='/inspector'>
+          <h2>Inspect</h2>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
