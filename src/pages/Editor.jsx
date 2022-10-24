994b9c507147ef7212dee8c1b3d93a8000ea6e09
Alaeddine Jmila <jmila.aladin@gmail.com>
2022-10-24 06:31:07 +0100

    Added and tested useFetch hook

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
+import { useFetch } from '../hooks/useFetch';
 import './Editor.css';
 
 const Editor = () => {
-  return <div className='editor'>Editor</div>;
+  const {
+    data: emotions,
+    loading,
+    error,
+  } = useFetch('http://localhost:3000/emotions');
+
+  return (
+    <>
+      <h2 className='editor'>Editor</h2>
+      {emotions && emotions.map(emo => <p key={emo.id}>{emo.name}</p>)}
+    </>
+  );
 };
 
 export default Editor;
