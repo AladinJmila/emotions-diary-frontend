78873fcb742a141c38e117ddfd0e42302460a60f
Alaeddine Jmila <jmila.aladin@gmail.com>
2022-11-06 14:58:21 +0000

    OK progress with data viz

--- src/components/OneWeekViz.jsx
+++ src/components/OneWeekViz.jsx
@@ -2,21 +2,8 @@ import * as d3 from 'd3';
 import { useEffect } from 'react';
 import { useEmoStates } from '../hooks/useEmoStates';
 
-function OneDayViz() {
+function OneWeekViz() {
   const { emoStates, loadEmoStates } = useEmoStates();
-
-  const shadesOfGrey = [
-    '#191919',
-    '#323232',
-    '#4B4B4B',
-    '#646464',
-    '#7D7D7D',
-    '#969696',
-    '#AFAFAF',
-    '#C8C8C8',
-    '#E1E1E1',
-    '#FAFAFA',
-  ];
 
   useEffect(() => {
     !emoStates.length && loadEmoStates();
@@ -41,24 +28,13 @@ function OneDayViz() {
       //   {
       name: 'cluster',
       children: [
-        { name: 'Abhorrence', value: 10000, color: shadesOfGrey[0] },
-        { name: 'Loathing', value: 2000, color: shadesOfGrey[5] },
-        { name: 'Sorrow', value: 5000, color: shadesOfGrey[3] },
-        { name: 'Ecstacy', value: 9000, color: shadesOfGrey[9] },
-        { name: 'Wonder', value: 4000, color: shadesOfGrey[6] },
-        { name: 'Grief', value: 3000, color: shadesOfGrey[2] },
-        { name: 'Excitement', value: 7000, color: shadesOfGrey[8] },
-        { name: 'loom', value: 5000, color: shadesOfGrey[5] },
-        { name: 'doom', value: 8000, color: shadesOfGrey[7] },
-        { name: 'Sorrow', value: 7000, color: shadesOfGrey[3] },
-        { name: 'Loathing', value: 1000, color: shadesOfGrey[5] },
-        { name: 'Wonder', value: 3000, color: shadesOfGrey[6] },
-        { name: 'Grief', value: 3000, color: shadesOfGrey[2] },
-        { name: 'Ecstacy', value: 5000, color: shadesOfGrey[9] },
-        { name: 'Abhorrence', value: 8000, color: shadesOfGrey[0] },
-        { name: 'loom', value: 10000, color: shadesOfGrey[5] },
-        { name: 'Excitement', value: 2000, color: shadesOfGrey[8] },
-        { name: 'goom', value: 7000, color: shadesOfGrey[5] },
+        { name: 'AgglomerativeCluster', value: 3938 },
+        { name: 'CommunityStructure', value: 3812 },
+        { name: 'HierarchicalCluster', value: 6714 },
+        { name: 'MergeEdge', value: 4578 },
+        { name: 'MergeEdge', value: 2222 },
+        { name: 'MergeEdge', value: 5555 },
+        { name: 'MergeEdge', value: 4183 },
         //   ],
         // },
         // {
@@ -101,10 +77,6 @@ function OneDayViz() {
       .select('#svg')
       .append('svg')
       .attr('viewBox', `-${width / 2} -${height / 2} ${width} ${height}`)
-      // .attr(
-      //   'viewBox',
-      //   `-${width / 2.55} -${height / 2.1} ${width * 0.85} ${height}`
-      // )
       .style('display', 'block')
       .style('margin', '0 -14px')
       .style('background', 'transparent')
@@ -116,31 +88,9 @@ function OneDayViz() {
       .selectAll('circle')
       .data(root.descendants().slice(1))
       .join('circle')
-      .attr('fill', d => d.data.color)
-      .attr('pointer-events', d => (!d.children ? 'none' : null))
-      // .attr('stroke', 'var(--color1)')
-      .attr('stroke', 'var(--bw-shade1)')
-      .attr('stroke-width', 25)
-      .on('mouseover', function () {
-        d3.select(this).attr('stroke', '#000');
-      })
-      .on('mouseout', function () {
-        d3.select(this).attr('stroke', null);
-      })
-      .on(
-        'click',
-        (event, d) => focus !== d && (zoom(event, d), event.stopPropagation())
-      );
-
-    const node2 = svg
-      .append('g')
-      .selectAll('circle')
-      .data(root.descendants().slice(1))
-      .join('circle')
-      .attr('fill', d => d.data.color)
+      .attr('fill', d => (d.children ? color(d.depth) : 'white'))
       .attr('pointer-events', d => (!d.children ? 'none' : null))
       .attr('stroke', 'var(--color1)')
-      // .attr('stroke', 'var(--bw-shade1)')
       .attr('stroke-width', 10)
       .on('mouseover', function () {
         d3.select(this).attr('stroke', '#000');
@@ -155,21 +105,14 @@ function OneDayViz() {
 
     const label = svg
       .append('g')
-      .style('font', '32px sans-serif')
+      .style('font', '24px sans-serif')
       .attr('pointer-events', 'none')
       .attr('text-anchor', 'middle')
-      .attr('stroke-width', 0)
       .selectAll('text')
       .data(root.descendants())
       .join('text')
       .style('fill-opacity', d => (d.parent === root ? 1 : 0))
       .style('display', d => (d.parent === root ? 'inline' : 'none'))
-      .attr('stroke', d =>
-        shadesOfGrey.slice(0, 5).includes(d.data.color) ? 'white' : 'black'
-      )
-      .attr('fill', d =>
-        shadesOfGrey.slice(0, 5).includes(d.data.color) ? 'white' : 'black'
-      )
       .text(d => d.data.name);
 
     zoomTo([root.x, root.y, root.r * 2]);
@@ -181,19 +124,13 @@ function OneDayViz() {
 
       label.attr(
         'transform',
-        d => `translate(${(d.x - v[0]) * k * 1},${(d.y - v[1]) * k * 1.8})`
+        d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
       );
       node.attr(
         'transform',
-        d => `translate(${(d.x - v[0]) * k * 1},${(d.y - v[1]) * k * 1.8})`
+        d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
       );
-      node.attr('r', d => d.r * k * 1);
-
-      node2.attr(
-        'transform',
-        d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k * 1.8})`
-      );
-      node2.attr('r', d => d.r * k * 0.89);
+      node.attr('r', d => d.r * k);
     }
 
     function zoom(event, d) {
@@ -229,4 +166,4 @@ function OneDayViz() {
   return <div id='svg'></div>;
 }
 
-export default OneDayViz;
+export default OneWeekViz;
